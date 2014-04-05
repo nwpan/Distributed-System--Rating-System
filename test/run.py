b24e513abf6ae721cb5c5f2ea4f6cb7476faab5c
Ted Kirkpatrick <ted@sfu.ca>
2014-04-05 15:41:55 -0700

    Add test suite feature to run.py
    run.py can once again run its full suite of tests. This is now its
    default behaviour.
    
    After every test, all the DB instances are terminated, the Q instance
    is cleared, and new DB instances are started. This eliminates pending queue
    entries from surviving one test to another, whether in the actual Q process
    or in buffers in the DB processes.
    
    This does not eliminate errors due to lingering state problems in the LB
    instance. That is left for another time.
    
    The commit also improves the wording of command-line help and fixes
    a bug that prevented the --wait option from having any effect.

--- test/run.py
+++ test/run.py
@@ -41,7 +41,7 @@ class HTTPOutput():
     def flush(self):
         pass
 
-parser = argparse.ArgumentParser(description='Process.')
+parser = argparse.ArgumentParser(description='Run single test or test suite.')
 
 parser.add_argument('--key',
                     dest='key',
@@ -60,13 +60,13 @@ parser.add_argument('--results',
 
 parser.add_argument('--leavedb',
                     action='store_true',
-                    help='leave the database after termination')
+                    help='leave the Redis servers running after termination')
 
 parser.add_argument('--test',
                     dest='test',
                     action='store',
                     nargs='?',
-                    default='simple',
+                    default=None,
                     help='name of single test to run')
 
 parser.add_argument('--ndb',
@@ -74,8 +74,8 @@ parser.add_argument('--ndb',
                     type=int,
                     action='store',
                     nargs='?',
-                    default=None,
-                    help='number of database nodes to spin up')
+                    default=4,
+                    help='number of database nodes to spin up; default %(default)s')
 
 parser.add_argument('--wait',
                     dest='wait',
@@ -83,7 +83,8 @@ parser.add_argument('--wait',
                     action='store',
                     nargs='?',
                     default=1,
-                    help='number of seconds to wait for servers to start')
+                    help=('number of seconds to wait for servers to start; '
+                          'occurs at start of every test; default %(default)s'))
 
 args = parser.parse_args()
 if args.output:
@@ -100,10 +101,7 @@ random.seed(args.key)
 
 ITEM = 'zoo'
 
-if args.ndb:
-    ndb = args.ndb
-else:
-    ndb = 4 # Number of database nodes
+ndb = args.ndb # Number of database nodes
 nlb = 1 # Number of load balancers
 nq = 1 # Number of queue servers
 digest_length = 2 # Number of PUTs in each digest
@@ -152,7 +150,7 @@ db_configs = [ {'id': i,
                 'qport': qs_base,
                 'digest-length': digest_length}
                 for i in range(ndb)]
-db_servers = [ subprocess.Popen(['python', os.path.join(base, 'serverDB.py'), json.dumps(config)]) for config in db_configs ]
+db_servers = []
 
 def endpoint(id, lb_port):
     return 'http://localhost:'+str(lb_port)+'/rating/'+id
@@ -218,6 +216,26 @@ def flush():
     for client in clients:
         client.flushall()
 
+def restartDBServers():
+    stopDBServers()
+    if len(db_servers) > 0: # Only drain queue if a test has been run
+        res = requests.delete('http://localhost:'+str(qs_base)+'/clear')
+    startDBServers()
+
+def startDBServers():
+    global db_servers
+    db_servers = [ subprocess.Popen(['python',
+                                     os.path.join(base, 'serverDB.py'),
+                                     json.dumps(config)])
+                                     for config in db_configs ]
+    # Give the server some time to start up
+    time.sleep(args.wait)
+
+
+def stopDBServers():
+    if len(db_servers) > 0:
+        for db_server in db_servers: db_server.terminate()
+
 def count():
     return sum(map(lambda c:c.info()['total_commands_processed'],clients))
 
@@ -246,9 +264,6 @@ print("Running test #"+args.key)
 result({ 'name': 'info', 'type': 'KEY', 'value': args.key })
 result({ 'name': 'info', 'type': 'SHARD_COUNT', 'value': ndb })
 
-# Give the server some time to start up
-time.sleep(1)
-
 tests = [ ]
 def test():
     def wrapper(f):
@@ -261,6 +276,7 @@ def test():
             info("Running test %s" % (f.__name__))
             # Clean the database before subsequent tests
             flush()
+            restartDBServers()
             # Reset the RNG to a known value
             random.seed(args.key+'/'+f.__name__)
             f(rx, *a)
@@ -317,13 +333,16 @@ def testGetGossip(result):
 
 # Go through all the tests and run them
 try:
+    # Start our servers
+    #startDBServers()
+
     for test in tests:
         if args.test == None or args.test == test.__name__:
             test()
 finally:
     # Shut. down. everything.
     for lb_server in lb_servers: lb_server.terminate()
-    for db_server in db_servers: db_server.terminate()
+    stopDBServers()
     for qs_server in qs_servers: qs_server.terminate()
     if not args.leavedb:
         for p in rd_processes: p.terminate()
